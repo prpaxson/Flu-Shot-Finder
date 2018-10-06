fb880a603a513844f8ec5dff256bdc37c04b24c2
prpaxson <prpaxson@gmail.com>
2018-10-06 15:18:20 -0700

    Some more backend server setup work

--- Podfile
+++ Podfile
@@ -1,5 +1,10 @@
+platform :ios, '12.0'
 source 'https://github.com/CocoaPods/Specs.git'
 target 'Flu Shot Finder' do
-pod 'GoogleMaps'
-pod 'GooglePlaces'
+    use_frameworks!
+    # <-- Map API Stuff Below -->
+    pod 'GoogleMaps'
+    pod 'GooglePlaces'
+    
+    pod 'AWSDynamoDB', '~> 2.6.13'
 end
--- Pods/Target Support Files/Pods-Flu Shot Finder/Pods-Flu Shot Finder-frameworks.sh
+++ Pods/Target Support Files/Pods-Flu Shot Finder/Pods-Flu Shot Finder-frameworks.sh
@@ -141,6 +141,15 @@ strip_invalid_archs() {
   STRIP_BINARY_RETVAL=1
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSDynamoDB/AWSDynamoDB.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSDynamoDB/AWSDynamoDB.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
