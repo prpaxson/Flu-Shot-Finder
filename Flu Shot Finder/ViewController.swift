456cc34c0bb0dc3e2fe77f2fd381d6e295658b8e
prpaxson <prpaxson@gmail.com>
2018-10-06 11:08:10 -0700

    Initial Commit

---
+++ Flu Shot Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flu Shot Finder
+//
+//  Created by Patrick Paxson on 10/6/18.
+//  Copyright © 2018 Patrick Paxson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
