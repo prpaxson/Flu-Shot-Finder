602a88cde36d265bfa6801b5acfb6a1cf875846b
Kevin Resler <kresler@its-sungod-mac4.ucsd.edu>
2018-10-06 19:40:53 -0700

    Basic Map functions like location and map movement. Need to only add markers for the places to nav to, and animations. Also only works if location is on. So also need to add search by zip

--- MapViewController.swift
+++ MapViewController.swift
@@ -14,31 +14,75 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     var currentLocation: CLLocation?
     var mapView: GMSMapView!
-
-    override func viewLoad() {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
         
-        let camera = GMSCameraPosition.camera(withLatitude: 1.285, longitude: 103.848, zoom: 12)
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+        
+    }
+
+//    override func viewLoad() {
+//
+//        let camera = GMSCameraPosition.camera(withLatitude: 1.285, longitude: 103.848, zoom: 12)
+//        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+//        do {
+//            // Set the map style by passing a valid JSON string.
+//            mapView.mapStyle = try GMSMapStyle(jsonString: basicMapStyle)
+//        } catch {
+//            NSLog("One or more of the map styles failed to load. \(error)")
+//        }
+//
+//        self.view = mapView
+//        mapView.isMyLocationEnabled = true
+//
+//
+//        // Do any additional setup after loading the view.
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        
+        let camera = GMSCameraPosition.camera(withLatitude: defaultLocation.coordinate.latitude,
+                                              longitude: defaultLocation.coordinate.longitude,
+                                              zoom: 10)
+        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.settings.myLocationButton = true
+        mapView.settings.rotateGestures = true
+        mapView.settings.zoomGestures = true
+        mapView.settings.scrollGestures = true
+        mapView.settings.tiltGestures = false
+
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
         do {
-            // Set the map style by passing a valid JSON string.
-            mapView.mapStyle = try GMSMapStyle(jsonString: basicMapStyle)
+            // Set the map style by passing the URL of the local file.
+            if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
+                mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+            } else {
+                NSLog("Unable to find style.json")
+            }
         } catch {
             NSLog("One or more of the map styles failed to load. \(error)")
         }
 
         self.view = mapView
         mapView.isMyLocationEnabled = true
-        
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func viewDidLoad() {
-        locationManager = CLLocationManager()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()()
-        locationManager.startUpdatingLocation()
-        locationManager.delegate = self
 
     }
     override func didReceiveMemoryWarning() {
@@ -46,26 +90,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    let basicMapStyle = "[" +
-        "  {" +
-        "    \"featureType\": \"poi.business\"," +
-        "    \"elementType\": \"all\"," +
-        "    \"stylers\": [" +
-        "      {" +
-        "        \"visibility\": \"off\"" +
-        "      }" +
-        "    ]" +
-        "  }," +
-        "  {" +
-        "    \"featureType\": \"transit\"," +
-        "    \"elementType\": \"labels.icon\"," +
-        "    \"stylers\": [" +
-        "      {" +
-        "        \"visibility\": \"off\"" +
-        "      }" +
-        "    ]" +
-        "  }" +
-    "]"
 
     
 
