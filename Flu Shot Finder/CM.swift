c2599be8cbb648a1588548eacbc5b8bf1438e571
Robert P Hernandez <rphernan@its-sungod-mac3.ucsd.edu>
2018-10-06 20:19:01 -0700

    Camera implemented hopefully

--- Flu Shot Finder/CM.swift
+++ Flu Shot Finder/CM.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class CM: UIViewController {
+class CM: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
-
+    @IBOutlet weak var myImg: UIImageView!
+    
+    @IBAction func capture(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+            imagePicker.allowsEditing = false
+            Self.presentViewController(imagePicker, animated: true, completion: nil)
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +30,14 @@ class CM: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            myImg.contentMode = .scaleToFill
+            myImg.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
     
 }
