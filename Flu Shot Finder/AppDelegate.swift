652c0a96505f9c3ab838ccbe19a14bcf62603820
prpaxson <prpaxson@gmail.com>
2018-10-06 21:50:00 -0700

    Fixing a random SIGABRT bug

--- Flu Shot Finder/AppDelegate.swift
+++ Flu Shot Finder/AppDelegate.swift
@@ -20,17 +20,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        // Create AWSMobileClient to connect with AWS
-        AWSDDLog.add(AWSDDTTYLogger.sharedInstance)
-        AWSDDLog.sharedInstance.logLevel = .info
         GMSServices.provideAPIKey("<#T##APIKey: String##String#>")
         GMSPlacesClient.provideAPIKey("YOUR_API_KEY")
-        return AWSMobileClient.sharedInstance().interceptApplication(
-            application,
-            didFinishLaunchingWithOptions: launchOptions)
-        
-        
-    }
+        // Create AWSMobileClient to connect with AWS
+        return true
+        }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
