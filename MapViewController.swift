fa8d3b843776d0d360ee46f8fbafaf564ece2e88
Kevin Resler <kresler@its-sungod-mac4.ucsd.edu>
2018-10-06 18:56:11 -0700

    Still working on the Map, I think I've added one but it has no location rn :)

--- MapViewController.swift
+++ MapViewController.swift
@@ -9,18 +9,64 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewLoad() {
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 1.285, longitude: 103.848, zoom: 12)
+        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        do {
+            // Set the map style by passing a valid JSON string.
+            mapView.mapStyle = try GMSMapStyle(jsonString: basicMapStyle)
+        } catch {
+            NSLog("One or more of the map styles failed to load. \(error)")
+        }
+
+        self.view = mapView
+        mapView.isMyLocationEnabled = true
+        
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidLoad() {
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()()
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    let basicMapStyle = "[" +
+        "  {" +
+        "    \"featureType\": \"poi.business\"," +
+        "    \"elementType\": \"all\"," +
+        "    \"stylers\": [" +
+        "      {" +
+        "        \"visibility\": \"off\"" +
+        "      }" +
+        "    ]" +
+        "  }," +
+        "  {" +
+        "    \"featureType\": \"transit\"," +
+        "    \"elementType\": \"labels.icon\"," +
+        "    \"stylers\": [" +
+        "      {" +
+        "        \"visibility\": \"off\"" +
+        "      }" +
+        "    ]" +
+        "  }" +
+    "]"
+
     
 
     /*
@@ -32,5 +78,4 @@ class MapViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
