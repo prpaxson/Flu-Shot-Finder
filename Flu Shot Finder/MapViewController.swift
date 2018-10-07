3c8a51e9f1ec9d6f2e5dd654deda833d65df2f05
Kevin Resler <kresler@its-sungod-mac4.ucsd.edu>
2018-10-06 18:07:29 -0700

    Here Is the Base of mY project, Before I pull The "BIG CHANGE"

---
+++ Flu Shot Finder/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  Flu Shot Finder
+//
+//  Created by Kevin Resler on 10/6/18.
+//  Copyright © 2018 Patrick Paxson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+
---
+++ MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  
+//
+//  Created by Kevin Resler on 10/6/18.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
