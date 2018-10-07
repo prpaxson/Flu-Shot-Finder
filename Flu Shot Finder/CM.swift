1cd2db673d8f1216edff6497bab16ab1e93dc5d5
Robert P Hernandez <rphernan@its-sungod-mac3.ucsd.edu>
2018-10-06 19:37:31 -0700

    VCC->CM

---
+++ Flu Shot Finder/CM.swift
@@ -0,0 +1,24 @@
+//
+//  CM.swift
+//  Flu Shot Finder
+//
+//  Created by Robert Hernandez on 10/6/18.
+//  Copyright © 2018 Robert Hernandez, . All rights reserved.
+//
+
+import UIKit
+
+class CM: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
---
+++ Flu Shot Finder/VCC.swift
@@ -0,0 +1,26 @@
+//
+//  VCC.swift
+//  Flu Shot Finder
+//
+//  Created by Robert Hernandez on 10/6/18.
+//  Copyright © 2018 Robert Hernandez, . All rights reserved.
+//
+
+import UIKit
+
+class VCC: UIViewController {
+    
+    @IBAction func completeShot(_ sender: Any) {
+        self.performSegueWithIdentifier("picTime", sender: self)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
